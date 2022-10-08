e35f79412b6746f70539dac63868b06c5cc4cd43
Dylan <57853417+4812571@users.noreply.github.com>
2022-10-08 15:14:34 -0400

    fix

--- mayhem/fuzz.py
+++ mayhem/fuzz.py
@@ -9,7 +9,7 @@ with atheris.instrument_imports():
 def TestOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
     in_str = fdp.ConsumeString(4096)
-    html2text(in_str)
+    html2text.html2text(in_str)
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
